fa6d6b2edb656782df32e2707ef2a7a11153c70f
vetlesen <jonas@vetlesen.no>
2023-08-17 22:38:12 +0200

    now?

--- sanity/preview.ts
+++ sanity/preview.ts
@@ -5,7 +5,6 @@ import { dataset, projectId } from './env'
 function onPublicAccessOnly() {
   throw new Error(`Unable to load preview as you're not logged in`)
 }
-
 export const usePreview = definePreview({
   projectId,
   dataset,
