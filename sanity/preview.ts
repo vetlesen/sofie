6975366a5797b092d8c9eb85e2037ae9986ee7c9
vetlesen <jonas@vetlesen.no>
2023-08-17 23:14:58 +0200

    kanskje?

--- sanity/preview.ts
+++ sanity/preview.ts
@@ -1,13 +1,13 @@
-import { definePreview } from 'next-sanity/preview'
+// import { definePreview } from 'next-sanity/preview'
 
-import { dataset, projectId } from './env'
+// import { dataset, projectId } from './env'
 
-function onPublicAccessOnly() {
-  throw new Error(`Unable to load preview as you're not logged in`)
-}
+// function onPublicAccessOnly() {
+//   throw new Error(`Unable to load preview as you're not logged in`)
+// }
 
-export const usePreview = definePreview({
-  projectId,
-  dataset,
-  onPublicAccessOnly,
-})
+// export const usePreview = definePreview({
+//   projectId,
+//   dataset,
+//   onPublicAccessOnly,
+// })
