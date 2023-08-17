6d110fb315bb7163c9998ecf26c61a5e6d879ea7
vetlesen <jonas@vetlesen.no>
2023-08-17 22:46:23 +0200

    plis

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,29 @@
-/**
-* This configuration file lets you run `$ sanity [command]` in this folder
-* Go to https://www.sanity.io/docs/cli to learn more.
-**/
+// /**
+// * This configuration file lets you run `$ sanity [command]` in this folder
+// * Go to https://www.sanity.io/docs/cli to learn more.
+// **/
+// import { defineCliConfig } from 'sanity/cli'
+
+// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+
+// export default defineCliConfig({ api: { projectId, dataset } })
+
+
+import { loadEnvConfig } from '@next/env'
 import { defineCliConfig } from 'sanity/cli'
 
+const dev = process.env.NODE_ENV !== 'production'
+loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
+
+// @TODO report top-level await bug
+// Using a dynamic import here as `loadEnvConfig` needs to run before this file is loaded
+// const { projectId, dataset } = await import('lib/sanity.api')
+
+// I do something wrong on the set up and add the dataset like this underneath..
+const dataset = "production"
+
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
 export default defineCliConfig({ api: { projectId, dataset } })
--- sanity/preview.ts
+++ sanity/preview.ts
@@ -5,6 +5,7 @@ import { dataset, projectId } from './env'
 function onPublicAccessOnly() {
   throw new Error(`Unable to load preview as you're not logged in`)
 }
+
 export const usePreview = definePreview({
   projectId,
   dataset,
