865d210b35827280d0cb9065c546b8ea28d7a5cd
vetlesen <jonas@vetlesen.no>
2023-12-12 22:27:26 +0100

    feat: plausible

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,13 @@
 import { Head, Html, Main, NextScript } from "next/document";
 import PlausibleProvider from "next-plausible";
 
-export default function Document(home) {
+export default function Document() {
   return (
     <Html lang="en" className="bg-white">
-      <Head>
+      <head>
         <PlausibleProvider domain="sofieramstad.no" />
+      </head>
+      <Head>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
